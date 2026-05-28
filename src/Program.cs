c72e0d1829b2041c12ea17a0027e3900a1da868d
Vu Minh Bach <vum0227@gmail.com>
2026-05-28 11:28:55 +0700

    Prevent duplicate app instances

--- src/Program.cs
+++ src/Program.cs
@@ -2,17 +2,42 @@ namespace SmoothScrollLocal;
 
 internal static class Program
 {
+    private const string SingleInstanceMutexName = "Local\\SmoothScrollLocal.SingleInstance";
+
     [STAThread]
     private static void Main()
     {
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
 
+        using var singleInstanceMutex = new Mutex(
+            initiallyOwned: true,
+            name: SingleInstanceMutexName,
+            createdNew: out var isFirstInstance);
+
+        if (!isFirstInstance)
+        {
+            MessageBox.Show(
+                "SmoothScroll Local da co mot ban dang chay truoc roi.",
+                "SmoothScroll Local",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            return;
+        }
+
         using var settingsStore = new SettingsStore();
         using var animator = new SmoothScrollAnimator(settingsStore);
         using var hook = new MouseHook(settingsStore, animator);
 
         hook.Start();
-        Application.Run(new TrayAppContext(settingsStore, hook));
+
+        try
+        {
+            Application.Run(new TrayAppContext(settingsStore, hook));
+        }
+        finally
+        {
+            singleInstanceMutex.ReleaseMutex();
+        }
     }
 }
